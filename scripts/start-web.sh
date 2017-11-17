9eb2cd5d620c3cff559e03c010a7e4deebbe861f
ihorman <ihorman@gmail.com>
2017-11-17 11:15:35 +0200

    Update start-web.sh

--- scripts/start-web.sh
+++ scripts/start-web.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sed -i '/cd \/data/a [ -f /data/web/index.html ] && { sed -i "s/VANILLA_VERSION/$VANILLA_VERSION/g" /data/web/index.html; pushd /data/web; screen -md python -m SimpleHTTPServer 8080 ; popd; }' /start-server.sh
+sed -i '/cd \/data/a [ -f /data/web/index.html ] && { sed -i "s/VANILLA_VERSION/$VANILLA_VERSION/g" /data/web/index.html; pushd /data/web; screen -md python -m SimpleHTTPServer 8080 ; popd; }' /start-minecraftFinalSetup
 
 
