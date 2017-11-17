5589a8daa44121072be022bdcb82bdacdbff2234
ihorman <ihorman@gmail.com>
2017-11-17 12:06:12 +0200

    typos are fixed

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -15,7 +15,7 @@ apk add screen
 
 VANILLA_VERSION=$(ls /data/minecraft_server* |  awk -F "." '{ print $2"."$3"."$4 }');
 
-[ ! -z $VANILA_VERSION ] && sed -i "s/VANILLA_VERSION/$VANILLA_VERSION/g" /data/web/index.html
+[ ! -z $VANILLA_VERSION ] && sed -i "s/VANILLA_VERSION/$VANILLA_VERSION/g" /data/web/index.html
 pushd /data/web; screen -md python -m SimpleHTTPServer 8080 ; popd;
 
 source /start
